6e34e6cfc51c9b599f25ac8c590bffb9c53568df
Kwon <dmsql662@gmail.com>
2016-04-13 12:02:24 +0900

    Xml

---
+++ Assets/Script/CardManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml; // Xml
+
+public class CardManager : MonoBehaviour
+{
+    public List<CardObject> Player_Card = new List<CardObject>();
+    public List<CardObject> Another_Card = new List<CardObject>();
+
+    void Awake ()
+    {
+        Player_Card = Setting("PlayerData");
+        Another_Card = Setting("PlayerData"); // 임시
+
+        // 카드 정보 파싱한 곳에서 찾아 데이터 집어넣기
+    }
+
+    public static List<CardObject> Setting(string filepath)
+    {
+        TextAsset data_xml = (TextAsset)Resources.Load(filepath, typeof(TextAsset));
+        XmlDocument Document = new XmlDocument();
+        Document.LoadXml(data_xml.text);
+
+        XmlElement InfoListElement = Document["Data"];
+
+        List<CardObject> CardList = new List<CardObject>();
+
+        //foreach (XmlElement InfoElement in InfoListElement.ChildNodes)
+        for (int i = 0; i < InfoListElement.ChildNodes.Count; i++)
+        {
+            CardObject Info = new CardObject();
+            Info.Name = System.Convert.ToString(InfoListElement.GetAttribute("Name"));
+
+            for(int j = 0; j < transform.Find("CardData").GetComponent<CardDataParse>.InfoList.Count; j++)
+            {
+                if(Info.Name == transform.Find("CardData").GetComponent<CardDataParse>.InfoList.Name)
+                {
+                    // 정보 넣기
+                    break;
+                }
+            }
+
+            CardList.Add(Info);
+        }
+
+
+
+        return CardList;
+    }
+}
---
+++ Assets/Script/CardObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum CARD_TYPE
+{
+    // 타입 추가
+};
+
+public class CardObject : MonoBehaviour
+{
+    private int m_Atk;
+    public int Atk
+    {
+        get { return m_Atk; }
+        set { m_Atk = Atk; }
+    }
+    private int m_Hp;
+    public int Hp
+    {
+        get { return m_Hp; }
+        set { m_Hp = Hp; }
+    }
+    private int m_Cost;
+    public int Cost
+    {
+        get { return m_Cost; }
+        set { m_Cost = Cost; }
+    }
+    private int m_Speed;
+    public int Speed
+    {
+        get { return m_Speed; }
+        set { m_Speed = Speed; }
+    }
+    private string m_Name;
+    public string Name
+    {
+        get { return m_Name; }
+        set { m_Name = Name; }
+    }
+    private string m_Text;
+    public string Text
+    {
+        get { return m_Text; }
+        set { m_Text = Text; }
+    }
+    private int m_Type;
+    public int Type
+    {
+        get { return m_Type; }
+        set { m_Type = Type; }
+    }
+}
