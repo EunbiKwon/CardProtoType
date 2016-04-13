ed3e9ed3f95853054998245ff482c80cbfbf8bef
Kwon <dmsql662@gmail.com>
2016-04-13 14:29:18 +0900

    GameManager

--- Assets/Script/CardManager.cs
+++ Assets/Script/CardManager.cs
@@ -5,18 +5,68 @@ using System.Xml; // Xml
 
 public class CardManager : MonoBehaviour
 {
-    public List<CardObject> Player_Card = new List<CardObject>();
-    public List<CardObject> Another_Card = new List<CardObject>();
+    // Singleton
+    private static CardManager instance;
 
-    void Awake ()
+    public static CardManager getInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new CardManager();
+            }
+            return instance;
+        }
+    }
+
+    public List<CardObject> Player_Card = new List<CardObject>();
+    public List<CardObject> Another_Card = new List<CardObject>();  // Temp
+
+    private void Awake ()
     {
         Player_Card = Setting("PlayerData");
-        Another_Card = Setting("PlayerData"); // 임시
-
-        // 카드 정보 파싱한 곳에서 찾아 데이터 집어넣기
+        Another_Card = Setting("PlayerData"); // Temp
     }
 
     public static List<CardObject> Setting(string filepath)
+    {
+        TextAsset data_xml = (TextAsset)Resources.Load(filepath, typeof(TextAsset));
+        XmlDocument Document = new XmlDocument();
+        Document.LoadXml(data_xml.text);
+
+        XmlElement InfoListElement = Document["Data"];
+
+        List<CardObject> SetList = new List<CardObject>(); // 사용자 카드
+        List<CardObject> CardList = new List<CardObject>(); // 카드 리스트
+
+        // Card Data Init
+        CardList = DataInfoRead("CardData");
+
+        // Player Card Info Setting
+        for (int i = 0; i < InfoListElement.ChildNodes.Count; i++)
+        {
+            CardObject Info = new CardObject();
+            // Card name list Input
+            Info.Name = System.Convert.ToString(InfoListElement.GetAttribute("Name"));
+
+            // Data Input
+            for(int j = 0; j < CardList.Count; j++)
+            {
+                if(Info.Name == CardList[j].Name)
+                {
+                    Info = CardList[j];
+                    break;
+                }
+            }
+
+            CardList.Add(Info);
+        }
+        
+        return SetList;
+    }
+
+    public static List<CardObject> DataInfoRead(string filepath)
     {
         TextAsset data_xml = (TextAsset)Resources.Load(filepath, typeof(TextAsset));
         XmlDocument Document = new XmlDocument();
@@ -31,21 +81,15 @@ public class CardManager : MonoBehaviour
         {
             CardObject Info = new CardObject();
             Info.Name = System.Convert.ToString(InfoListElement.GetAttribute("Name"));
-
-            for(int j = 0; j < transform.Find("CardData").GetComponent<CardDataParse>.InfoList.Count; j++)
-            {
-                if(Info.Name == transform.Find("CardData").GetComponent<CardDataParse>.InfoList.Name)
-                {
-                    // 정보 넣기
-                    break;
-                }
-            }
+            Info.Cost = System.Convert.ToInt32(InfoListElement.GetAttribute("Cost"));
+            Info.Speed = System.Convert.ToInt32(InfoListElement.GetAttribute("Speed"));
+            Info.Atk = System.Convert.ToInt32(InfoListElement.GetAttribute("Atk"));
+            Info.Type = System.Convert.ToInt32(InfoListElement.GetAttribute("AtkType"));
+            Info.Text = System.Convert.ToString(InfoListElement.GetAttribute("Text"));
 
             CardList.Add(Info);
+            Debug.Log(Info.Name);
         }
-
-
-
         return CardList;
     }
 }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-	void Awake ()
+    const int ROUND = 6;
+    const int SPEED = 6;
+
+    private CardObject Player_Main;     // Player Main Character
+    private CardObject Another_Main;    // Other Main Character
+    private List<CardObject> Player_List = new List<CardObject>();   // Player field card
+    private List<CardObject> Another_List = new List<CardObject>();   // Other field card
+
+    void Awake ()
     {
-	
+        Init();
 	}
 	
 	void Update ()
     {
-	
+	    // 게임 순서 Init -> Battle -> Change
+        // 그 뒤로는 메인 캐릭이 체력이 0이하가 될 때까지 Battle -> Change 반복
+
+        while( Player_Main.Hp > 0 && Another_Main.Hp > 0 )
+        {
+            for (int i = 0; i < ROUND; i++)
+            {
+                Battle(i);
+            }
+        }
 	}
+
+    void Init()
+    {
+        Player_Main = new CardObject();
+        Another_Main = new CardObject();
+
+        // 플레이어 카드 선택 일단 보류
+        // 프로토타입이라 일단 리스트 가져오겟슴
+        Player_List = CardManager.getInstance.Player_Card;
+        Another_List = CardManager.getInstance.Another_Card;
+    }
+
+    void Battle(int _round)
+    {
+        for(int i = 0; i < SPEED; i++)
+        {
+            // 선공 안정해서 일단 플레이어부터 공격
+            if(0 == (Player_List[i].Speed % (i+1)))
+            {
+
+            }
+        }
+    }
+
+    void Change()
+    {
+
+    }
 }
